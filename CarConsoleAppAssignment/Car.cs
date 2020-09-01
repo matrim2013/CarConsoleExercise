d04f3042146813cc0d061d37bc05066608c86e1d
Lewis S <simpkinslewisa@gmail.com>
2020-09-01 15:18:33 -0400

    Class Excerise

---
+++ CarConsoleAppAssignment/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarConsoleAppAssignment
+{
+    public class Car
+    {
+        public Car() { }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ CarConsoleAppAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCar = new Car();
+            newCar.Make = "Chevy";
+            newCar.Model = "Impala";
+            newCar.Year = 2000;
+
+            var abesCar = new Car("Tesla", "Model Y", 2020);
+
+            var carList = new List<Car>() { abesCar, newCar };
+
+            Console.WriteLine($"I have a {newCar.Make} {newCar.Model} from {newCar.Year}");
+            Console.WriteLine($"My dream car is a {abesCar.Year} {abesCar.Make} {abesCar.Model}");
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
+            }
+        }
+    }
+}
+
